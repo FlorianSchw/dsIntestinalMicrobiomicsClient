d2c49707ff4ab8105e7bbeeebb89b02e9b57d055
FlorianSchw <Florian.Schwarz@dife.de>
2022-10-11 09:15:49 +0200

    general update

---
+++ R/ds.microbiomeIFAA.R
@@ -0,0 +1,85 @@
+#'
+#' @title Computes the association of microbiome data with covariates
+#' @description This function is similar to the native R function from the IFAA package
+#' @details The function calls the server-side function \code{microbiomeIFAADS} that computes the
+#' association analysis from a SummarizedExperiment object. SummarizedExperiment objects can be computed using the \code{ds.summarizedExperiment} function.
+#' @param SumExp is a string character describing the SummarizedExperiment object
+#' @param covariates is a string character of covariates to be examined along the microbiome variables (can also be a vector of covariates).
+#' @param confounders is a string character for the covariates that will be adjusted in the model (can also be a vector of microbiome variables)
+#' @param sampleIDname is a string character for the sample ID variable.
+#' @param covariatesMany is a logical. If 'TRUE' and 'covariates' are set to NULL, then all variables in the 'covariates' will be used.
+#' @param confoundersMany is a logical. If 'TRUE' and 'confounders' are set to NULL, then all variables except the 'coviariates' will be used as confounders.
+#' @param nRef number of randomly picked reference taxa used in phase 1.
+#' @param nRefMaxForEsti maximum number of final reference taxa used in phase 2.
+#' @param taxa vector of taxa or OTU or ASV names. Theses are reference taxa specified by the user to be used in phase 1.
+#' @param adjust_method The adjusting method for p value adjustment. Default is "BY" for dependent FDR adjustment. It can take any adjustment method for the p.adjust function in R.
+#' @param fdrRate The false discovery rate for identifying taxa/OTU/ASV associated with 'covariates'.
+#' @param paraJobs If 'sequentialRun' is FALSE, this specifies the number of parallel jobs that will be registered to run the algoithm. If specified as NULL, it will automatically detect the cores to decide the number of parallel jobs.
+#' @param bootB number of bootstrap samples for obtaining confidence interval of estimates in phase 2 for the high dimensional regression. Default is 500.
+#' @param standardize is a logical. If 'TRUE', the design matrix for X will be standardized in the analyses and the results. Default is FALSE.
+#' @param sequentialRun is a logical. Defines whether there are parallel jobs or not.
+#' @param refReadsThresh The threshold of proportion of non-zero sequencing reads for choosing the reference taxon in phase 2. Default is 0.2 meaning that at least 20% non-zero sequencing reads are necessary.
+#' @param taxDropThresh The threshold of number of non-zero sequencing reads for each taxon to be dropped from the analysis. Default is 0 which means that taxon without any sequencing reads will be dropped from the analysis.
+#' @param SDTresh The threshold of standard deviations of sequencing reads for being chosen as the reference taxon in phase 2. The default is 0.05 which means the standard deviation of sequencing reads should be at least 0.05 in order to be chosen as a reference taxon.
+#' @param SDquantilThresh The threshold of the quantile of standard deviation of sequencing reads, above which could be selected as a reference taxon. Default is 0.
+#' @param balanceCut The threshold of the proportion of non-zero sequencing reads in each group of a binary variable for choosing the final reference taxa in phase 2. The default is 0.2 meaning at least 20% non-zero sequencing reads in each group are needed to be eligible for being chosen as a final reference taxon.
+#' @param verbose Whether the process message is printed out to the console. Default is TRUE.
+#' @param seed Random seed for reproducibility. Can be set to NULL to remove seeding.
+#' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login
+#' @return XXXXX
+#' @author Florian Schwarz for the German Institute of Human Nutrition
+#' @import DSI
+#' @import dsBaseClient
+#' @import methods
+#' @export
+#'
+
+ds.microbiomeIFAA <- function(SumExp = NULL, covariates = NULL, confounders = NULL, sampleIDname = NULL, covariatesMany = TRUE, confoundersMany = FALSE,
+                              nRef = 40, nRefMaxforEsti = 2, taxa = NULL, adjust_method = "BY", fdrRate = 0.15, paraJobs = NULL, bootB = 500,
+                              standardize = FALSE, sequentialRun = FALSE, refReadsThresh = 0.2, taxDropThresh = 0, SDThresh = 0.05, SDquantilThresh = 0,
+                              balanceCut = 0.2, verbose = TRUE, seed = 1, datasources = NULL){
+
+
+  # look for DS connections
+  if(is.null(datasources)){
+    datasources <- datashield.connections_find()
+  }
+
+
+  # ensure datasources is a list of DSConnection-class
+  if(!(is.list(datasources) && all(unlist(lapply(datasources, function(d) {methods::is(d,"DSConnection")}))))){
+    stop("The 'datasources' were expected to be a list of DSConnection-class objects", call.=FALSE)
+  }
+
+
+  if(is.null(SumExp)){
+    stop("Please provide the name of the SummarizedExperiment object!", call.=FALSE)
+  }
+
+
+
+  # call the internal function that checks the input object is of the same class in all studies.
+  typ <- dsBaseClient::ds.class(SumExp, datasources)
+
+
+
+  # Check whether the input is either of type data frame or matrix
+  if(!('SummarizedExperiment' %in% typ)){
+    stop("Only objects of type 'SummarizedExperiment' are allowed.", call.=FALSE)
+  }
+
+
+
+  # call the server side function that does the operation
+  cally <- call("microbiomeIFAADS", SumExp, covariates, confounders, sampleIDname, covariatesMany, confoundersMany, nRef, nRefMaxforEsti, taxa, adjust_method,
+                fdrRate, paraJobs, bootB, standardize, sequentialRun, refReadsThresh, taxDropThresh, SDThresh, SDquantilThresh, balanceCut, verbose, seed)
+  DSI::datashield.aggregate(datasources, cally)
+
+
+
+}
+
+# aggregate function
+
+
+
--- R/ds.microbiomeMZILN.R
+++ R/ds.microbiomeMZILN.R
@@ -6,8 +6,18 @@
 #' @param SumExp is a string character describing the SummarizedExperiment object
 #' @param taxa is a string character for the microbiome variable denominator (can also be a vector of microbiome variables)
 #' @param covariates is a string character of covariates to be examined along the microbiome variables (can also be a vector of covariates).
+#' @param sampleIDname is a string character for the sample ID variable.
+#' @param adjust_method The adjusting method for p value adjustment. Default is "BY" for dependent FDR adjustment. It can take any adjustment method for the p.adjust function in R.
+#' @param fdrRate The false discovery rate for identifying taxa/OTU/ASV associated with 'covariates'.
+#' @param paraJobs If 'sequentialRun' is FALSE, this specifies the number of parallel jobs that will be registered to run the algoithm. If specified as NULL, it will automatically detect the cores to decide the number of parallel jobs.
+#' @param bootB Number of bootstrap samples for obtaining confidence interval of estimates for the high dimensional regression.Default is 500.
+#' @param taxDropThresh The threshold of number of non-zero sequencing reads for each taxon to be dropped from the analysis. Default is 0 which means that taxon without any sequencing reads will be dropped from the analysis.
+#' @param standardize is a logical. If TRUE, the design matrix for X will be standardized in the analyses and the results. Default is FALSE.
+#' @param sequentialRun is a logical. Default is TRUE. It can be set to be FALSE to increase speed if there are multiple taxa in the argument 'taxa'.
+#' @param verbose Whether the process message is printed out to the console. Default is TRUE.
+#' @param seed Random seed for reproducibility. Can be set to NULL to remove seeding.
 #' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login
-#' @return XXXXX
+#' @return \code{ds.microbiomeMZILN} returns XXXXXXXXXXXXXXXXXXXXXX
 #' @author Florian Schwarz for the German Institute of Human Nutrition
 #' @import DSI
 #' @import dsBaseClient
@@ -15,7 +25,8 @@
 #' @export
 #'
 
-ds.microbiomeMZILN <- function(SumExp = NULL, taxa = NULL, covariates = NULL, datasources = NULL){
+ds.microbiomeMZILN <- function(SumExp = NULL, taxa = NULL, covariates = NULL, sampleIDname = NULL, adjust_method = "BY", fdrRate = 0.15, paraJobs = NULL,
+                               bootB = 500, taxDropThresh = 0, standardize = FALSE, sequentialRun = TRUE, verbose = TRUE, seed = 1, datasources = NULL){
 
 
   # look for DS connections
@@ -54,7 +65,7 @@ ds.microbiomeMZILN <- function(SumExp = NULL, taxa = NULL, covariates = NULL, da
 
 
   # call the server side function that does the operation
-  cally <- call("microbiomeMZILNDS", SumExp, taxa, covariates)
+  cally <- call("microbiomeMZILNDS", SumExp, taxa, covariates, sampleIDname, adjust_method, fdrRate, paraJobs, bootB, taxDropThresh, standardize, sequentialRun, verbose, seed)
   DSI::datashield.aggregate(datasources, cally)
 
 
@@ -62,19 +73,5 @@ ds.microbiomeMZILN <- function(SumExp = NULL, taxa = NULL, covariates = NULL, da
 }
 
 # aggregate function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
